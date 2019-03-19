30e23905743861297f799824eb4d0b4da010e040
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2019-03-19 15:09:23 -0700

    added flask

---
+++ Python/readme.md
@@ -0,0 +1,11 @@
+## Requirements
+
+``
+$ pip install -r requirements.txt
+``
+
+## Run the development server
+
+``
+$ python server/index.py
+``
---
+++ Python/server/app.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask(__name__)
+# Creates the Flask application object, which contains data about the application
+# and also methods (object functions) that tell the application to do certain actions.
+
+@app.route('/<test>', methods=['GET'])
+def home(test):
+    return '<h1>'+ test +'</h1>'
+
+app.run(port=8000, debug=True)
+# A method that runs the application server
