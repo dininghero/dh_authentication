7fb93a2b2aec75f06c4c73f827347defabc7a86d
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2019-03-21 02:33:49 -0700

    added dotenv package, created keygen file, started work on key gen function

---
+++ NodeJS/server/utilities/jwtKeyGenerator.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+// make a function to change secret keys
+/**
+ * After a set amount of time, secret keys will rotate and a new JWT token will need to created
+ * @function
+ * @return {String} - new key thay has been selected to be used
+*/
+const rotateKeys = () => {
+
+};
