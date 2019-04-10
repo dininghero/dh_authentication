2a11c2d6999357f982c30fdec01cface99540bd8
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2019-04-10 16:27:31 -0700

    added util functions and made new route

---
+++ NodeJS/server/middlewares/rupr.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const rupr = express.Router();
+
+/**
+  * Restaurant Username Password Recovery
+  * URL: '/rupr'
+  * Method: POST
+  * Headers: { Content-Type: application/json }
+  * Data Params: {  }
+  */
+rupr.route('rupr').post((req, res) => {
+  
+});
+
+module.exports = rupr;
